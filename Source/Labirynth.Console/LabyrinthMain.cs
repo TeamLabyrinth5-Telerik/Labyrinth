e65e7d1515b88b66fd093707dfff15db4e797bec
vlad-sp <vladimirsopotlyanov@gmail.com>
2015-09-19 14:51:49 +0300

    Renamed Labirynth to Labyrinth.

---
+++ Source/Labirynth.Console/LabyrinthMain.cs
@@ -0,0 +1,19 @@
+﻿namespace Labyrinth.Console
+{
+    using Labyrinth.Logic;
+    using Labyrinth.Logic.Interfaces;
+    using Labyrinth.Models;
+
+    public class LabyrinthMain
+    {
+        public static void Main()
+        {
+            IUserInterface userInterface = new ConsoleInterface();
+            IRenderer renderer = new ConsoleRenderer();
+            IInitializer initializer = new Initializer();
+
+            Engine engine = new Engine(renderer, userInterface, initializer);
+            engine.Run();
+        }
+    }
+}
