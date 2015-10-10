c118084fb129be59116ddf0abe33058827cebc80
vlad-sp <vladimirsopotlyanov@gmail.com>
2015-10-10 09:37:58 +0300

    Added missing file

---
+++ Source/Labyrinth.Tests/TestScoreboard.cs
@@ -0,0 +1,54 @@
+﻿namespace Labyrinth.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Models;
+    using Telerik.JustMock;
+    using Models.Interfaces;
+    using System.Linq;
+
+    [TestClass]
+    public class TestScoreboard
+    {
+        private Scoreboard scoreboard;
+        private Player player;
+
+        [TestMethod]
+        public void ShouldReturnZeroWhenHaveEmptyList()
+        {
+            scoreboard = new Scoreboard();
+            var actual = scoreboard.Players.Count;
+            var expected = 0;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldAddPlayerToList()
+        {
+            scoreboard = new Scoreboard();
+            player = new Player();
+            scoreboard.AddPlayer(player);
+
+            var actual = scoreboard.Players.Count;
+            var expected = 1;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldAddPlayerWithNameToList()
+        {
+            scoreboard = new Scoreboard();
+            player = new Player("Stamat");
+            scoreboard.AddPlayer(player);
+
+            var expected = player.Name;
+            var actual = scoreboard.Players.FirstOrDefault().Name;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
