3e5d17ebf4c89f6b557b2238568c6c40a318bca0
AdrianApostolov <adrian.apostolov.1991@gmail.com>
2015-10-12 10:05:24 +0300

    Add some test for Engine

---
+++ Source/Labyrinth.Tests/Logic/TestLabyrinthEngine.cs
@@ -0,0 +1,36 @@
+﻿namespace Labyrinth.Tests.Logic
+{
+    using Labyrinth.Console.Interfaces;
+    using Labyrinth.Logic;
+    using Labyrinth.Logic.Interfaces;
+    using Labyrinth.Models.Interfaces;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class TestLabyrinthEngine
+    {
+        [TestMethod]
+        public void LabyrinthEngineRunMethodShouldBeInvokedOnce()
+        {
+            var mockedEngine = new Mock<IEngine>();
+            mockedEngine.Setup(x => x.Run());
+            mockedEngine.Object.Run();
+            mockedEngine.Verify(x => x.Run(), Times.Exactly(1));
+        }
+
+        [TestMethod]
+        public void EngineShouldBeInstanceOfTypeIEngine()
+        {
+            var fakeInitializer = new Mock<IInitializer>().Object as IInitializer;
+            var fakeUserInterface = new Mock<IUserInterface>().Object as IUserInterface;
+            var fakeRenderer = new Mock<IRenderer>().Object as IRenderer;
+            var fakePlayer = new Mock<IPlayer>().Object as IPlayer;
+            var fakeGrid = new Mock<IGrid>().Object as IGrid;
+
+            var engine = new LabyrinthEngine(fakeRenderer, fakeUserInterface, fakeInitializer, fakePlayer, fakeGrid);
+
+            Assert.IsInstanceOfType(engine, typeof(IEngine));
+        }
+    }
+}
